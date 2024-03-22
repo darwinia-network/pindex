2768fa7981466c0e8d5c16a4907d5ad8f77d7c11
Aki Wu <wuminzhe@gmail.com>
2024-03-22 17:50:27 +0800

    add shadcn ui(without using typescript) by using its cli

--- app/javascript/app.jsx
+++ app/javascript/app.jsx
@@ -1,3 +1,14 @@
 import * as React from 'react'
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
 
-export default () => <h1>Hello, React!</h1>
+
+export default () => {
+  return  (
+    <Alert>
+      <AlertTitle>Hello, ShadcnUI</AlertTitle>
+      <AlertDescription>
+        You can add components and dependencies to your app using the cli.
+      </AlertDescription>
+    </Alert>
+  )
+}
--- app/javascript/controllers/hello_controller.jsx
+++ app/javascript/controllers/hello_controller.jsx
@@ -1,11 +1,12 @@
 import { Controller } from "@hotwired/stimulus"
 
 import * as React from 'react'
-import * as ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./../app"
 
 export default class extends Controller {
   connect() {
-    ReactDOM.render(<App />, this.element);
+    const root = ReactDOM.createRoot(this.element);
+    root.render(<App />);
   }
 }
